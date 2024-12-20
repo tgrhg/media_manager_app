1ff4183fd4cdf2652d061cebf035e8824aa73088
grhg <52474584+tgrhg@users.noreply.github.com>
2024-12-21 00:58:16 +0900

    :sparkles: add search page

---
+++ lib/services/rakuten_api.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class RakutenAPI {
+  static const String apiKey = '[API Key]'; // 取得したAPIキーをここに入れる
+  static const String endpoint =
+      'https://app.rakuten.co.jp/services/api/BooksDVD/Search/20170404';
+
+  static Future<List<dynamic>> searchItems(String query) async {
+    final url = Uri.parse(
+        '$endpoint?format=json&formatVersion=2&title=$query&applicationId=$apiKey&sort=-releaseDate');
+
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      return data['Items'] ?? [];
+    } else {
+      throw Exception('Failed to load data from Rakuten API');
+    }
+  }
+}
