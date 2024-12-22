fe6203b69b2781e592cc7803a8f4c71046e28c29
grhg <52474584+tgrhg@users.noreply.github.com>
2024-12-22 20:35:48 +0900

    :rotating_light: fix lint error

--- lib/main.dart
+++ lib/main.dart
@@ -126,6 +126,8 @@ class MyAppState extends ChangeNotifier {
 }
 
 class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
